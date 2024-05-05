77fa1f863b5d0390f525086ac8b008d1bf389d80
Park Juhyung(Borre) <jh@majecty.com>
2024-05-06 00:15:09 +0900

    get json node information

--- giu/src/cmd/blockchainexplorer/explorer/nodetab.go
+++ giu/src/cmd/blockchainexplorer/explorer/nodetab.go
@@ -1,16 +1,41 @@
 package explorer
 
 import (
+	"context"
+	"encoding/json"
+
 	g "github.com/AllenDang/giu"
 )
 
 var (
-	nodeInformation string
+	nodeStatus string
 )
 
 func DrawNodeWidgets() []g.Widget {
 	return []g.Widget{
 		g.Label("Node information"),
-		g.InputTextMultiline(&nodeInformation).Size(-1, -1),
+		g.Button("Get Node information").OnClick(func() {
+			nodeStatus = ""
+			httpClient, err := getTendermintHTTPClient()
+			if err != nil {
+				SetGlobalError(err)
+				return
+			}
+
+			nodeStatusResult, err := httpClient.Status(context.Background())
+			if err != nil {
+				SetGlobalError(err)
+				return
+			}
+
+			nodeStatusJson, err := json.Marshal(nodeStatusResult)
+			if err != nil {
+				SetGlobalError(err)
+				return
+			}
+
+			nodeStatus = string(nodeStatusJson)
+		}),
+		g.InputTextMultiline(&nodeStatus).Size(-1, -1),
 	}
 }
